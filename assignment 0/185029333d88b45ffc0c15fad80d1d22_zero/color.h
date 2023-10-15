03ad0d18baf7518f25a86ebc407f539ba776691e
Gabriele Federico <gabrielefederico@live.it>
2023-10-15 13:32:57 +0200

    restore files and extract color in header

---
+++ assignment 0/185029333d88b45ffc0c15fad80d1d22_zero/color.h
@@ -0,0 +1,20 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "Vector4f.h"
+
+struct Color
+{
+	Vector4f RGBA;
+	float* as_float_arr()
+	{
+        float arr[4];
+        arr[0] = RGBA.x();
+        arr[1] = RGBA.y();
+        arr[2] = RGBA.z();
+        arr[3] = RGBA.w();
+        return arr;
+	}
+};
+
+#endif
